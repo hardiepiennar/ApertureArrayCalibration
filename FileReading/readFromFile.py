Scaling is now correct, still need to get absolute values correct. NF2FF function now in single function

FileReading/readFromFile.py
@@ -235,7 +235,7 @@ def parse_angle_range_line(line):
     return float(elements[1]), float(elements[2]), float(elements[3])
 
 
-def read_frequency_block_from_spherical_dataset(block, no_samples, theta, phi, gain_theta, gain_phi):
+def read_frequency_block_from_farfield_dataset(block, no_samples, theta, phi, gain_theta, gain_phi):
     """
     Extract a single frequency block from the dataset farfield data
     :param block: block no to extract
@@ -291,7 +291,8 @@ def transform_data_coord_to_grid(x_length, y_length, coord_data):
     grid = np.reshape(coord_data, (y_length, x_length))
     return grid
 
-def get_phi_cut(trace_no, no_samples, gain):
+
+def get_phi_cut_from_coord_data(trace_no, no_samples, gain):
     """
     Return a phi cut given a theta data set number
     :param trace_no: cut number in dataset
@@ -305,3 +306,15 @@ def get_phi_cut(trace_no, no_samples, gain):
     phi_cut = np.array(phi_cut)
 
     return phi_cut
+
+
+def get_phi_cut_from_grid_data(trace_no, gain):
+    """
+    Return a phi cut given a theta data set number
+    :param gain: 2D matrix of gain grid values
+    :param trace_no: cut number in dataset
+    :return: phi cut vector
+    """
+    gain_coord = np.reshape(gain,(1,len(gain)*len(gain[0])))[0]
+    no_samples = [1, len(gain[0]), len(gain)]
+    return get_phi_cut_from_coord_data(trace_no, no_samples, gain_coord)

FileReading/test_readFromFile.py
@@ -179,7 +179,7 @@ class TestFEKOFileUtilities(unittest.TestCase):
                     26, 25, 24, 23, 22, 21, 20, 19, 18]
         no_samples = [3, 3, 3]
         block_no = 0
-        theta_block, phi_block, gain_theta_block, gain_phi_block = rFF.read_frequency_block_from_spherical_dataset(
+        theta_block, phi_block, gain_theta_block, gain_phi_block = rFF.read_frequency_block_from_farfield_dataset(
             block_no,
             no_samples,
             theta,
@@ -229,22 +229,36 @@ class TestFEKOFileUtilities(unittest.TestCase):
         self.assertEqual(y_data_block, [1, 7, 6, 5, 4, 3, 2, 1, 0])
         self.assertEqual(z_data_block, [2, 7, 6, 5, 4, 3, 2, 1, 0])
 
-    def testExtractPhiCut(self):
+    def testExtractPhiCutFromCoordData(self):
         theta = np.array([0, 1, 2, 0, 1, 2, 0, 1, 2])
         phi = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
         gain = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
         no_samples = np.array([1, 3, 3])
 
-        theta_block_no = 0
-        cut = rFF.get_phi_cut(theta_block_no, no_samples, gain)
-        self.assertEqual(cut[0], [0])
-        self.assertEqual(cut[2], [2])
+        phi_block_no = 0
+        cut = rFF.get_phi_cut_from_coord_data(phi_block_no, no_samples, gain)
+        self.assertEqual(cut[0], 0)
+        self.assertEqual(cut[2], 2)
 
-        theta_block_no = 2
-        cut = rFF.get_phi_cut(theta_block_no, no_samples, gain)
+        phi_block_no = 2
+        cut = rFF.get_phi_cut_from_coord_data(phi_block_no, no_samples, gain)
         self.assertEqual(cut[0], 6)
         self.assertEqual(cut[2], 8)
 
+    def testExtractPhiCutFromGridData(self):
+        theta = np.array([[0, 1, 2], [0, 1, 2], [0, 1, 2]])
+        phi = np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]])
+        gain = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+
+        phi_block_no = 0
+        cut = rFF.get_phi_cut_from_grid_data(phi_block_no, gain)
+        self.assertEqual(cut[0], 0)
+        self.assertEqual(cut[2], 2)
+
+        phi_block_no = 2
+        cut = rFF.get_phi_cut_from_grid_data(phi_block_no, gain)
+        self.assertEqual(cut[0], 6)
+        self.assertEqual(cut[2], 8)
 
     def test_transform_data_from_coord_to_grid_form(self):
         """Test the transformation of data from coordinate form to grid form"""

NF2FF/NF2FF.py
@@ -85,23 +85,67 @@ def calculate_total_e_field(ex, ey, ez):
     return e
 
 
-def calc_nf2ff(nearfield_x, nearfield_y):
-    """
-    Calculates the farfield given the nearfield data
-    :param grid_x: 2D matrix with x coords
-    :param grid_y: 2D matrix with y coords
-    :param grid_z: 2D matrix with z coords
-    :param nearfield_x: 2D matrix of complex nearfield_x values
-    :param nearfield_y: 2D matrix of complex nearfield_y values
-    :param nearfield_z: 2D matrix of complex nearfield_z values
-    :return farfield_x, farfield_y, farfield_z: complex farfield 2D matrices
+def calc_nf2ff(freq, x_grid, y_grid, ex_grid, ey_grid, theta_grid, phi_grid, verbose=False, pad_factor=4):
     """
 
-    """Calculate the x and y farfields with the fourier transform"""
-    farfield_x = np.fft.fftshift(np.fft.ifft2(nearfield_x))
-    farfield_y = np.fft.fftshift(np.fft.ifft2(nearfield_y))
+    :param freq: frequency in Hz
+    :param x_grid: 2D grid matrix of x coordinates in m
+    :param y_grid: 2D grid matrix of y coordinates in m
+    :param ex_grid: 2D grid matrix of x directed nearfield values V/m
+    :param ey_grid: 2D grid matrix of y directed nearfield values V/m
+    :param theta_grid: 2D grid matrix of theta points to calculate in rad
+    :param phi_grid: 2D grid matrix of phi points to calculate in rad
+    :param verbose: Give information during method
+    :param pad_factor: amount of padding to add during angular spectrum calculation
+    :return e_theta, e_phi: theta and phi farfield values for given coordinates
+    """
+    if verbose:
+        print("Probing the ether for its fundamental constants")
+    frequency = freq
+    lambda0 = calc_freespace_wavelength(frequency)
+    wavenumber = calc_freespace_wavenumber(frequency)
+    if verbose:
+        print("Frequency:  "+str(frequency/1e6)+" MHz")
+        print("Wavelength: "+str(np.round(lambda0, 3))+" m")
+        print("Wavenumber: "+str(np.round(wavenumber, 3))+" rad/m\n")
 
-    return farfield_x, farfield_y
+    if verbose:
+        print("Increasing angular spectrum resolution with a zero padding factor of: "+str(pad_factor))
+    x_grid_pad, y_grid_pad, ex_grid_pad, ey_grid_pad, _ = pad_nearfield_grid(x_grid, y_grid,
+                                                                             ex_grid, ey_grid, [],
+                                                                             pad_factor)
+
+    if verbose:
+        print("Generating k-space")
+    kx_grid, ky_grid, _ = generate_kspace(x_grid_pad, y_grid_pad, wavenumber)
+
+    if verbose:
+        print("Calculating angular spectrum of nearfield..."),
+    fex_grid = calc_angular_spectrum(ex_grid_pad)
+    fey_grid = calc_angular_spectrum(ey_grid_pad)
+    if verbose:
+        print("[DONE]")
+
+    if verbose:
+        print("Interpolating angular spectrum data onto spherical grid..."),
+    fex_spherical = interpolate_cartesian_to_spherical(kx_grid, ky_grid, fex_grid, wavenumber,
+                                                       theta_grid[0], phi_grid[:, 0])*np.sqrt(2)*len(fex_grid)**2
+    fey_spherical = interpolate_cartesian_to_spherical(kx_grid, ky_grid, fey_grid, wavenumber,
+                                                       theta_grid[0], phi_grid[:, 0])*np.sqrt(2)*len(fex_grid)**2
+    if verbose:
+        print("[DONE]")
+
+    if verbose:
+        print("Calculating theta and phi components..."),
+    r = 10000
+    C = calc_propagation_coef(frequency, r)
+    e_theta, e_phi = transform_cartesian_to_spherical(theta_grid, phi_grid, fex_spherical, fey_spherical)
+    e_theta *= C
+    e_phi *= C
+    if verbose:
+        print("[DONE]\n")
+
+    return e_theta, e_phi
 
 
 def generate_kspace(grid_x, grid_y, wavenumber):
@@ -135,9 +179,10 @@ def calc_angular_spectrum(nearfield):
     :return farfield: angular spectrum
     """
 
-    farfield = np.fft.fftshift(np.fft.ifft2(nearfield))
+    farfield = np.fft.ifftshift(np.fft.ifft2(nearfield))
 
     return farfield
+
 
 def generate_spherical_theta_phi_grid(theta_steps, phi_steps, theta_lim, phi_lim):
     """
@@ -218,7 +263,6 @@ def calc_propagation_coef(freq, distance):
     """
     k0 = calc_freespace_wavenumber(freq)
     C = 1j*(k0*np.exp(-1j*k0*distance))/(2*np.pi*distance)
-
     return C
 
 
@@ -240,6 +284,7 @@ def pad_nearfield_grid(grid_x, grid_y, nearfield_x, nearfield_y, nearfield_z, pa
 
     return grid_x, grid_y, nearfield_x, nearfield_y, nearfield_z
 
+
 def interpolate_cartesian_to_spherical(kx_grid, ky_grid, fe_grid, wavenumber, theta, phi):
     """
     :param kx_grid: wavenumber x grid
@@ -250,19 +295,19 @@ def interpolate_cartesian_to_spherical(kx_grid, ky_grid, fe_grid, wavenumber, th
     :param phi: phi points to calculate in rad
     :return: angular spectrum in spherical grid
     """
-    fe_spherical_real_func = interpolate.RectBivariateSpline(kx_grid[0], ky_grid[:, 0], np.real(fe_grid))
-    fe_spherical_imag_func = interpolate.RectBivariateSpline(kx_grid[0], ky_grid[:, 0], np.imag(fe_grid))
+    fe_spherical_real_func = interpolate.RectBivariateSpline(kx_grid[0], ky_grid[:, 0], np.real(np.conj(fe_grid)))
+    fe_spherical_imag_func = interpolate.RectBivariateSpline(kx_grid[0], ky_grid[:, 0], np.imag(np.conj(fe_grid)))
 
     fe_spherical = np.reshape(np.zeros(len(phi)*len(theta), dtype=complex), (len(phi), len(theta)))
 
     for phi_i in np.arange(len(phi)):
         for theta_i in np.arange(len(theta)):
-            x_coord = wavenumber*np.sin(theta[theta_i])*np.cos(phi[phi_i])
-            y_coord = wavenumber*np.sin(theta[theta_i])*np.sin(phi[phi_i])
-            fe_spherical[phi_i][theta_i] = complex(fe_spherical_real_func(x_coord, y_coord)[0]) - \
-                1j*complex(fe_spherical_imag_func(x_coord, y_coord)[0])
-
+            y_coord = wavenumber*np.sin(theta[theta_i])*np.cos(phi[phi_i])
+            x_coord = wavenumber*np.sin(theta[theta_i])*np.sin(phi[phi_i])
+            fe_spherical[phi_i][theta_i] = complex(fe_spherical_real_func(x_coord, y_coord)) + \
+                1j*complex(fe_spherical_imag_func(x_coord, y_coord))
     return fe_spherical
+
 
 def calc_dft2(x, y, z, data):
     """

NF2FF/test_NF2FF.py
@@ -93,37 +93,6 @@ class NF2FFTestCases(unittest.TestCase):
             for y in np.arange(len(test_trans_data[0])):
                 self.assertEqual(trans_data[x][y], test_trans_data[x][y])
 
-    def test_nearfield_to_farfield_xyz(self):
-        nearfield_x = np.array([[0, 0, 0], [0, 2, 0], [0, 0, 0]])
-        nearfield_y = np.array([[0, 0, 0], [0, 0.1, 0], [0, 0, 0]])
-        scale = (float(1)/10)
-
-        N = 3
-        d = 2
-
-        farfield_test_x = (float(1)/N**2)*2*np.array([[np.exp(-2j*np.pi*-1*d/N - 2j*np.pi*-1*d/N),
-                                                        np.exp(-2j*np.pi*0*d/N - 2j*np.pi*-1*d/N),
-                                                        np.exp(-2j*np.pi*1*d/N - 2j*np.pi*-1*d/N)],
-                                                        [np.exp(-2j*np.pi*-1*d/N - 2j*np.pi*0*d/N),
-                                                        np.exp(-2j*np.pi*0*d/N - 2j*np.pi*0*d/N),
-                                                        np.exp(-2j*np.pi*1*d/N - 2j*np.pi*0*d/N)],
-                                                        [np.exp(-2j*np.pi*-1*d/N - 2j*np.pi*1*d/N),
-                                                        np.exp(-2j*np.pi*0*d/N - 2j*np.pi*1*d/N),
-                                                        np.exp(-2j*np.pi*1*d/N - 2j*np.pi*1*d/N)]])
-
-
-        farfield_test_y = 0.1*farfield_test_x/2
-
-        trans_farfield_x, trans_farfield_y = nf2ff.calc_nf2ff(nearfield_x, nearfield_y)
-
-        """Test if the transforms were done correctly for the x and y farfields"""
-        for y in np.arange(len(farfield_test_x)):
-            for x in np.arange(len(farfield_test_x[0])):
-                self.assertAlmostEqual(trans_farfield_x[y][x], farfield_test_x[y][x])
-        for y in np.arange(len(trans_farfield_y)):
-            for x in np.arange(len(trans_farfield_y[0])):
-                self.assertAlmostEqual(trans_farfield_y[y][x], farfield_test_y[y][x])
-
     def test_generate_kspace(self):
         x_grid = 0.1*np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
         y_grid = 0.1*np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
@@ -335,38 +304,33 @@ class NF2FFTestCases(unittest.TestCase):
         
         fe_spherical = nf2ff.interpolate_cartesian_to_spherical(kx_grid,ky_grid,fe_grid,wavenumber,theta,phi)
 
-        fe_spherical_test = np.array([[-1.19798150562089 + 19.3797511507843j,
-                                      -0.227964875625187 - 2.07083872882152j,
-                                      -7.98042564710115 + 4.81284813513545j,
-                                      -3.47463478357622 + 4.12300261398233j,
-                                      34.5652524272203 - 43.4613894696073j],
-                                     [0.316884874378104 + 0.192356769257137j,
-                                      -0.0115539438318393 + 0.148913211027039j,
-                                      -7.98042564710115 + 4.81284813513545j,
-                                      -0.173370941001693 + 3.70191446043908j,
-                                      -0.166156570604876 + 2.86011902866362j],
-                                     [-1.94415044619004 + 15.1691583332975j,
-                                      0.134466179122087 - 1.69826826556644j,
-                                      -7.98042564710115 + 4.81284813513545j,
-                                      -1.94169113866461 + 4.60548677858829j,
-                                      24.7469933111242 - 43.1344041703480j],
-                                     [-0.265038684720100 - 0.953366740538459j,
-                                      -0.0153679320156506 - 0.440670149314655j,
-                                      -7.98042564710115 + 4.81284813513545j,
-                                      0.126694991242735 - 1.08728625015510j,
-                                      -0.742216143509634 - 0.841813991415576j],
-                                     [34.5652524272203 - 43.4613894696072j,
-                                      -3.47463478357622 + 4.12300261398232j,
-                                      -7.98042564710115 + 4.81284813513545j,
-                                      -0.227964875625189 - 2.07083872882153j,
-                                      -1.19798150562087 + 19.3797511507843j]])
+        self.assertAlmostEqual(fe_spherical[0][0], 0)
+        self.assertAlmostEqual(fe_spherical[3][3], -0.01536793201565383-0.44067014931466802j)
+        self.assertAlmostEqual(fe_spherical[1][2], -7.9804256471011605+4.8128481351354457j)
+        self.assertAlmostEqual(fe_spherical[-1][-1], 0)
 
-        self.assertEqual(len(fe_spherical), len(fe_spherical_test))
-        self.assertEqual(len(fe_spherical[0]), len(fe_spherical_test[0]))
+    def test_nf2ff(self):
+        freq = 1e9
+        grid_x = np.array([[-2, -1, 0, 1, 2], [-2, -1, 0, 1, 2], [-2, -1, 0, 1, 2], [-2, -1, 0, 1, 2],
+                           [-2, -1, 0, 1, 2]])
+        grid_y = np.array([[-2, -2, -2, -2, -2], [-1, -1, -1, -1, -1], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1],
+                           [2, 2, 2, 2, 2]])
+        nf_x_grid = np.array([[0, 0, 0, 0, 0], [0, 0, 2, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
+        nf_y_grid = np.array([[0, 0, 0, 0, 0], [0 ,0, 0.1, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
+        theta = np.linspace(-np.pi/4, np.pi/4, 3)
+        phi = np.linspace(0, np.pi, 3)
+        theta_grid, phi_grid = np.meshgrid(theta, phi)
 
-        for y in np.arange(0, len(fe_spherical_test)):
-            for x in np.arange(1,len(fe_spherical_test[0])-1):
-                self.assertAlmostEqual(fe_spherical_test[y][x], fe_spherical[y][x])
+        e_theta, e_phi = nf2ff.calc_nf2ff(freq, grid_x, grid_y, nf_x_grid, nf_y_grid, theta_grid, phi_grid)
+        self.assertAlmostEqual(e_theta[0][0], 0.00062603852060522309-0.00070579709530884727j)
+        self.assertAlmostEqual(e_theta[0][1], 0.00081313743687209971-0.00047758266899805595j)
+        self.assertAlmostEqual(e_theta[1][2], -4.3892978727040892e-05+1.7225453461635786e-05j)
+        self.assertAlmostEqual(e_theta[2][0], -0.00092133292603638209+0.00020302563490658213j)
+
+        self.assertAlmostEqual(e_phi[0][0], 2.2133804160197362e-05-2.4953695611732681e-05j)
+        self.assertAlmostEqual(e_phi[0][1], 4.0656871843604999e-05-2.38791334499028e-05j)
+        self.assertAlmostEqual(e_phi[2][2], -2.2133804160197379e-05+2.4953695611732786e-05j)
+        self.assertAlmostEqual(e_phi[1][0], 0.0005150812656806909-0.00042350118706216801j)
 
     def end(self):
         pass

pyNF2FF.py
@@ -12,24 +12,25 @@ import NF2FF.NFFFandFPlotting as plotting
 import matplotlib.pyplot as plt
 
 """General settings"""
-filename = "WaveguideHorn80deg.efe"
+filename_nearfield = "WaveguideHorn85deg.efe"
+filename_farfield = "WaveguideHorn.ffe"
 separation = 0.0899377374000528
 frequency_block = 0  # Select the first frequency block in the file
 pad_factor = 4
 
 
 # Spherical farfield pattern settings
-scan_angle = np.deg2rad(1)
+scan_angle = np.deg2rad(0)
 theta_lim = (-np.pi/2+scan_angle, np.pi/2-scan_angle)
 phi_lim = (0, np.pi)
-theta_steps = 31
-phi_steps = 31
+theta_steps = 101
+phi_steps = 101
 
 """Start of script"""
 print("\nStarting pyNF2FF\n")
 
-print("Importing nearfield data from "+filename)
-f, x, y, z, ex, ey, ez, coord_structure = rff.read_fekonearfield_datafile(filename)
+print("Importing nearfield data from " + filename_nearfield)
+f, x, y, z, ex, ey, ez, coord_structure = rff.read_fekonearfield_datafile(filename_nearfield)
 
 # Assuming an equally spaced 2D grid calculate grid properties (only 1 z-layer is currently permitted)
 delta = np.abs(x[1]-x[0])
@@ -47,7 +48,6 @@ print("Separation:  "+str(round(separation, 3))+" m\n")
 print("Extracting block: "+str(frequency_block)+" from imported dataset\n")
 x, y, z, ex, ey, ez = rff.read_frequency_block_from_nearfield_dataset(frequency_block, coord_structure, x, y, z,
                                                                       ex, ey, ez)
-
 print("Probing the ether for its fundamental constants")
 c0, e0, u0 = nf2ff.get_fundamental_constants()
 frequency = f[frequency_block*(x_points*y_points)]
@@ -64,57 +64,85 @@ ex_grid = rff.transform_data_coord_to_grid(x_points, y_points, ex)
 ey_grid = rff.transform_data_coord_to_grid(x_points, y_points, ey)
 ez_grid = rff.transform_data_coord_to_grid(x_points, y_points, ez)
 
-print("Increasing angular spectrum resolution with a zero padding factor of: "+str(pad_factor))
-x_grid_pad, y_grid_pad, ex_grid_pad, ey_grid_pad, ez_grid_pad = nf2ff.pad_nearfield_grid(x_grid, y_grid,
-                                                                                         ex_grid, ey_grid, ez_grid,
-                                                                                         pad_factor)
-
-print("Generating k-space")
-kx_grid, ky_grid, kz_grid = nf2ff.generate_kspace(x_grid_pad, y_grid_pad, wavenumber)
-
 print("Generating theta phi spherical grid\n")
 theta_grid, phi_grid = nf2ff.generate_spherical_theta_phi_grid(theta_steps, phi_steps, theta_lim, phi_lim)
 
-print("Calculating angular spectrum of nearfield..."),
-fex_grid = nf2ff.calc_angular_spectrum(ex_grid_pad)
-fey_grid = nf2ff.calc_angular_spectrum(ey_grid_pad)
-fez_grid = -(kx_grid*fex_grid + ky_grid*fey_grid)/kz_grid
-print("[DONE]")
+print("Starting NF2FF transform...")
+e_theta, e_phi = nf2ff.calc_nf2ff(frequency, x_grid, y_grid, ex_grid, ey_grid, theta_grid, phi_grid, verbose=True)
 
-print("Interpolating angular spectrum data onto spherical grid..."),
-fex_spherical = nf2ff.interpolate_cartesian_to_spherical(kx_grid, ky_grid, fex_grid, wavenumber,
-                                                         theta_grid[0], phi_grid[:, 0])
-fey_spherical = nf2ff.interpolate_cartesian_to_spherical(kx_grid, ky_grid, fey_grid, wavenumber,
-                                                         theta_grid[0], phi_grid[:, 0])
-fez_spherical = nf2ff.interpolate_cartesian_to_spherical(kx_grid, ky_grid, fez_grid, wavenumber,
-                                                         theta_grid[0], phi_grid[:, 0])
-print("[DONE]")
 
-print("Calculating theta and phi components..."),
-r = 10000
-C = nf2ff.calc_propagation_coef(frequency, r)
-e_theta, e_phi = nf2ff.transform_cartesian_to_spherical(theta_grid, phi_grid, fex_spherical, fey_spherical)
-e_theta = C*e_theta
-e_phi = C*e_phi
-e = nf2ff.calculate_total_e_field(e_theta, e_phi,0)
-print("[DONE]")
+e = 1/(2*120*np.pi)*(e_theta*np.conj(e_theta)+e_phi*np.conj(e_phi))
+mag_e = 10*np.log10(np.abs(e))
+#mag_e = 20 + mag_e - np.max(mag_e)
+print("[DONE]\n")
 
-print("Plotting data")
-mag_e = 20*np.log10(np.abs(e))
-plotting.plot_nearfield_2d_all(x_grid, y_grid, ex_grid, ey_grid, ez_grid, "Nearfield")
-plotting.plot_farfield_kspace_2d_all(kx_grid, ky_grid, fex_grid, fey_grid, fez_grid, "Farfield")
-plotting.plot_farfield_3d_spherical(theta_grid, phi_grid, mag_e, "Farfield")
-plotting.plot_farfield_3d_cartesian(theta_grid, phi_grid, mag_e, "Farfield")
+print("Importing farfield data from " + filename_nearfield)
+f, theta_ff, phi_ff, e_theta_ff, e_phi_ff, coord_structure_ff = rff.read_fekofarfield_datafile(filename_farfield)
+
+# Assuming an equally spaced 2D grid calculate grid properties (only 1 z-layer is currently permitted)
+e_theta_points = coord_structure_ff[1]
+e_phi_points = coord_structure_ff[2]
+delta_theta = np.abs(theta_ff[1]-theta_ff[0])
+delta_phi = np.abs(phi_ff[e_theta_points]-phi_ff[0])
+
+print("File imported with following structure:")
+print("Frequencies:   "+str(coord_structure[0]))
+print("Theta-Points:  "+str(e_theta_points))
+print("Phi-Points:    "+str(e_phi_points))
+print("Theta-Step:    "+str(delta_theta)+" deg")
+print("Phi-Step:      "+str(delta_phi)+" deg\n")
+
+print("Extracting block: "+str(frequency_block)+" from imported dataset\n")
+theta_ff, phi_ff, e_theta_ff, e_phi_ff = rff.read_frequency_block_from_farfield_dataset(frequency_block,
+                                                                                        coord_structure_ff,
+                                                                                        np.deg2rad(theta_ff),
+                                                                                        np.deg2rad(phi_ff),
+                                                                                        e_theta_ff, e_phi_ff)
+
+print("Transforming data into meshgrid")
+theta_grid_ff = rff.transform_data_coord_to_grid(e_theta_points, e_phi_points, theta_ff)
+phi_grid_ff = rff.transform_data_coord_to_grid(e_theta_points, e_phi_points, phi_ff)
+e_theta_grid_ff = rff.transform_data_coord_to_grid(e_theta_points, e_phi_points, e_theta_ff)
+e_phi_grid_ff = rff.transform_data_coord_to_grid(e_theta_points, e_phi_points, e_phi_ff)
+e_ff = nf2ff.calculate_total_gain(e_theta_grid_ff, e_phi_grid_ff)
+
+print("Plotting data..."),
+if(False):
+    plotting.plot_nearfield_2d_all(x_grid, y_grid, ex_grid, ey_grid, ez_grid, "Nearfield")
+    plotting.plot_farfield_kspace_2d_all(kx_grid, ky_grid, fex_grid, fey_grid, fez_grid, "Farfield")
+
+    plotting.plot_farfield_3d_spherical(theta_grid, phi_grid, mag_e, "Transformed Farfield")
+    plotting.plot_farfield_3d_cartesian(theta_grid, phi_grid, mag_e, "TransformedFarfield")
+
+    plotting.plot_farfield_3d_cartesian(theta_grid_ff, phi_grid_ff, e_ff, "FEKO Farfield")
+    plotting.plot_farfield_3d_spherical(theta_grid_ff, phi_grid_ff, e_ff, "FEKO Farfield")
+
+if(True):
+    e_cut_nf = rff.get_phi_cut_from_grid_data(50, mag_e-np.max(mag_e))
+    e_cut_ff = rff.get_phi_cut_from_grid_data(50, e_ff-np.max(e_ff))
+    plt.figure()
+    plt.plot(np.rad2deg(theta_grid[50]), e_cut_nf)
+    plt.plot(np.rad2deg(theta_grid_ff[0]), e_cut_ff)
+    plt.xlim(-90, 90)
+    plt.ylim(-60, 0)
+    plt.grid(True)
+    plt.axvline(-60)
+    plt.axvline(60)
+    plt.axvline(-80)
+    plt.axvline(80)
+
+print("[DONE]")
 
 plt.show()
 
 # TODO: Check scaling with some principle cuts
 # TODO: Program comparison vectors
 # TODO: Import real farfield
-# TODO: Compare farfields
 # TODO: Decide on comparison methodology
 # TODO: Decide on errors to focus on
 # TODO: Write error injection code
 # TODO: Write parameter sweep code
 # TODO: Run comparisons for all error types
 # TODO: Refactor NFFFandFPlotting
+# TODO: Add some 2D phase unwrapping?
+# TODO: Sort out scaling factor